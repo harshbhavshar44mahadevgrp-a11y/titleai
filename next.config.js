af807bb2011b41f335ec893a4116fb360f4db163
Harsh Bhavshar <rathodvrund248@gmail.com>
2026-06-08 12:53:07 +0530

    increase body size limit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
+    typescript: { ignoreBuildErrors: true },
+    eslint: { ignoreDuringBuilds: true },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '10mb',
+        },
     },
 }
-
 module.exports = nextConfig
