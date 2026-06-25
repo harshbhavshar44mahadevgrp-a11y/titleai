271ad339bb72f7e5788488c5fa2e7fddf108cc1e
Harsh Bhavshar <rathodvrund248@gmail.com>
2026-06-25 14:23:07 +0530

    fix: next.config.js for Next.js 16

--- next.config.js
+++ next.config.js
@@ -3,14 +3,5 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '20mb',
-    },
-  },
 }
-
 module.exports = nextConfig
